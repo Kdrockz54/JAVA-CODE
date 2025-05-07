ec66be9be1535ca687a1c3743721038af61a2aad
KUTBUDDIN (KD) <128181801+Kdrockz54@users.noreply.github.com>
2025-05-07 12:33:32 +0530

    Update minMaxSum method in JAVA.java

--- minMaxSum method in JAVA.java
+++ minMaxSum method in JAVA.java
@@ -1,5 +1,6 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+//Given five positive integers, find the minimum and maximum values that can be calculated by summing 
+  //  exactly four of the five integers. Then print the respective minimum and maximum values as a 
+    //single line of two space-separated long integers.
 import java.util.Scanner;
 
 
